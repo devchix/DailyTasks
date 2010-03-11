d045e985343ae0a2fe7fbfe32b753ffecbb94a35
Rebecca <somerset@gmail.com>
2010-03-10 23:39:04 -0800

    got some stuff working

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  def list
+    @tasks = Task.all
+    
+     respond_to do |format|
+        format.html # list.html.erb
+        format.xml  { render :xml => @tasks }
+      end
+      
+  end
+
+
+
+  def edit
+  end
+
+  def details
+  end
+  
+ 
+   def add
+      @task = Task.new(params[:task])
+      respond_to do |format|
+        if @task.save
+          flash[:notice] = 'Task was successfully created.'
+          format.html 
+          format.xml  { render :xml => @task}
+        else
+          format.html 
+          format.xml  { render :xml => @task.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
+ 
+
+end
