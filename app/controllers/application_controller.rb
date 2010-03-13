86ee1cfc62eee1bafcbeba27cb99ad63400b7822
Nola Stowe <nolastowe@nola.local>
2010-03-12 20:44:58 -0600

    made it even more simple

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
   filter_parameter_logging :password, :password_confirmation
-  helper_method :current_user_session, :current_user
+  helper_method :current_user
 
   private
     # For right now, using rails default session
-    def current_user_session
-      @current_user_session = session
-    end
-
     def current_user
-      return session[:user] if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.user
+      return session[:user] || User.new
     end
 end
