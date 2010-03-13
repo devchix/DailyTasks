59fae3a01a1ae6473782856330bcf786a572c666
Nola Stowe <nolastowe@nola.local>
2010-03-12 20:28:44 -0600

    super simple user authentication system for prototype

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  filter_parameter_logging :password, :password_confirmation
+  helper_method :current_user_session, :current_user
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  private
+    # For right now, using rails default session
+    def current_user_session
+      @current_user_session = session
+    end
+
+    def current_user
+      return session[:user] if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.user
+    end
 end
