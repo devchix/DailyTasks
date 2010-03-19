28198361b78f006561d1ce6eb716a7b0210bce2b
Rebecca <somerset@gmail.com>
2010-03-18 20:30:14 -0700

    clean up the base actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,24 +6,45 @@ class TasksController < ApplicationController
         format.html # list.html.erb
         format.xml  { render :xml => @tasks }
       end
-      
+  end
+  
+  def show
+    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def create
     @task = Task.new(params[:task])
     if @task.save
       flash[:notice] = 'Task was successfully created.'
-      redirect_to tasks_url
-    else
-     #error saving
+      redirect_to(@task) 
     end
   end
 
-  def edit
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update_attributes(params[:task])
+        flash[:notice] = 'Task was successfully updated.'
+     end
+     redirect_to @task
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.xml
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    redirect_to(tasks_url)
+
   end
 
 end
