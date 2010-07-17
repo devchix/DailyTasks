269f648d21d72a0e3ba41b935631a29066203dbe
Rebecca <somerset@gmail.com>
2010-07-16 19:00:27 -0700

    add validation of email address (hard coded list for now)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   
    has_many :tasks
+   def validate
+        valid_addresses = [ "somerset@gmail.com", "rebecca.gutterman@gmail.com", "mrnicksgirl@gmail.com", "beverly@beverlynelson.net"]
+        if valid_addresses.include? email
+        else
+          message =  email+" not in whitelist, application is restricted to approved users during beta."
+          errors.add("email ",message)
+        end
+   end
 end
