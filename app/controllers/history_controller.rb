58c14e24f07f51e1e2b9a6517cec4ea612060364
Nola Stowe <nolastowe@nola.gateway.2wire.net>
2010-07-22 21:43:17 -0500

    removed code lingering from google map api

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -9,14 +9,6 @@ class HistoryController < ApplicationController
       :conditions => ["task_id=:task_id and completed_on>:week_ago",{:task_id => params[:id],:week_ago => (Date.today-7)}])
   
     else
-      chart = GoogleChart.new
-      chart.type = :line_xy
-      chart.height = 150
-      chart.width = 300
-      chart.labels = ['one', 'two','three','four']
-      chart.data = [10,5,10,2]
-      @graph_url = chart.to_url
-      @message = "Placeholder : put cumulative stats here!"
       
       @completions = TaskHistory.find_for_week(current_user.id)
      
