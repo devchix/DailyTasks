2764d0d452bca1364ddba706a8ddd8a8794a3aaa
Nola Stowe <nolastowe@nola.gateway.2wire.net>
2010-07-29 21:38:58 -0500

    added some style and work on query for tasks

--- app/models/task_history.rb
+++ app/models/task_history.rb
@@ -1,8 +1,11 @@
 class TaskHistory < ActiveRecord::Base
   belongs_to :task
 
-  def self.find_completed_for_week
-TaskHistory.find_by_sql("select count(*) total, completed_on from task_histories group by completed_on").collect{ |t| t.total.to_i}
+  def self.find_completed_for_week(user_id, today = Date.today)
+    #gets the days between the week
+    start_of_week = today - (today.cwday) - 1  
+    end_of_week = start_of_week + 7.days 
+TaskHistory.find_by_sql("select count(*) total, completed_on from task_histories LEFT JOIN tasks ON task_histories.task_id=tasks.id WHERE tasks.user_id=#{user_id} and task_histories.completed_on BETWEEN '#{start_of_week}' and '#{end_of_week}' group by completed_on ").collect{ |t| t.total.to_i}
   end
   
   def self.find_for_week(user_id)
@@ -10,4 +13,5 @@ TaskHistory.find_by_sql("select count(*) total, completed_on from task_histories
       :conditions => [" task_histories.completed_on>:week_ago and tasks.user_id=:user_id",{:week_ago => (Date.today-7),:user_id => user_id}],
       :joins => "inner join tasks on tasks.id=task_histories.task_id")
   end
+  
 end
--- lib/tasks/devchix.rake
+++ lib/tasks/devchix.rake
@@ -4,10 +4,11 @@ namespace :devchix do
     args.with_default(:task => 'test task') 
     test_user = User.first
     task = test_user.tasks.create(:name => args.task)
+    task.complete(Date.today - 10.day)    
     task.complete(Date.today - 3.day)
     task.complete(Date.today - 2.day)
     task.complete(Date.today - 1.day)
-    task.complete() #default does today
+    task.complete #default does today
   
     puts "added task #{args.task}"
   end
